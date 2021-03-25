bd9ee1d5415b9a1bb4cb63b6f09769e342475b9d
michael-brine <70493009+michael-brine@users.noreply.github.com>
2021-03-24 18:25:07 -0700

    Wrote Parser Skeleton

--- parser.cc
+++ parser.cc
@@ -1,3 +1,77 @@
 #include "parser.h"
 
-struct InstructionNode* Parser::parse_generate_intermediate_representation() {}
+struct InstructionNode* Parser::parse_generate_intermediate_representation() {
+    parse_program();
+}
+void Parser::parse_program() {
+
+}
+void Parser::parse_var_section() {
+
+}
+void Parser::parse_var_section() {
+
+}
+void Parser::parse_id_list() {
+
+}
+void Parser::parse_body() {
+
+}
+void Parser::parse_stmt_list() {
+
+}
+void Parser::parse_stmt() {
+
+}
+void Parser::parse_assign_stmt() {
+
+}
+void Parser::parse_expr() {
+
+}
+void Parser::parse_primary() {
+
+}
+void Parser::parse_op() {
+
+}
+void Parser::parse_output_stmt() {
+
+}
+void Parser::parse_input_stmt() {
+
+}
+void Parser::parse_while_stmt() {
+    
+}
+void Parser::parse_if_stmt() {
+    
+}
+void Parser::parse_condidtion() {
+    
+}
+void Parser::parse_relop() {
+        
+}
+void Parser::parse_switch_stmt() {
+
+}
+void Parser::parse_for_stmt() {
+
+}
+void Parser::parse_case_list() {
+
+}
+void Parser::parse_case() {
+
+}
+void Parser::parse_defualt_case() {
+
+}
+void Parser::parse_inputs() {
+
+}
+void Parser::parse_num_list() {
+    
+}
